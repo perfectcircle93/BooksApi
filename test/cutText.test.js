ce0abbdb3b8635566b55768ebcb175e6010ff47f
Magdalena Świderska <m.swiderska@outlook.com>
2020-11-01 02:09:04 +0100

    Add test 2 - return an errot ig content arg length is 0

--- test/cutText.test.js
+++ test/cutText.test.js
@@ -1,6 +1,8 @@
 const cutText = require('../utils/cutText.js');
 const expect = require('chai').expect;
 
+
+/*test 1*/ 
 it('should return an error if "content" arg is not a string', () => {
   expect(cutText(undefined, 20)).to.equal('Error');
   expect(cutText(12, 20)).to.equal('Error');
@@ -8,3 +10,8 @@ it('should return an error if "content" arg is not a string', () => {
   expect(cutText([], 20)).to.equal('Error');
   expect(cutText(function() {}, 20)).to.equal('Error');
 });
+
+/*test 2*/
+it('should return an error if "content" arg length is 0', () => {
+    expect(cutText('', 20)).to.equal('Error');
+  });
