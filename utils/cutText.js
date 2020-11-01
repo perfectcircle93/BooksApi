90e9f5799933be1667706ca768fd5eeebbab32b6
Magdalena Świderska <m.swiderska@outlook.com>
2020-11-01 02:13:25 +0100

    Add test 3 - return error if maxLength is not a number

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,5 +1,9 @@
 module.exports = (content, maxLength) => {
-  if(typeof content !== 'string') return 'Error';
+  if(
+      typeof content !== 'string' ||
+      typeof maxLength !== 'number'
+    ) 
+      return 'Error';
   if(content.length < 1) return 'Error';
   if(content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
